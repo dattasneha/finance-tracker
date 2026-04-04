459cac6c72d595b4d06fcbddf8594722f396ba24
Sneha Datta <snehadatta431@gmail.com>
2026-04-04 21:30:29 +0530

    create the record apis

--- src/controllers/records.controller.js
+++ src/controllers/records.controller.js
@@ -23,4 +23,50 @@ const createRecords = asyncHandler(async (req, res) => {
   )
 });
 
-export {createRecords};
+const viewRecords = asyncHandler(async (req, res) => {
+    const userId = req.user.id;
+    const records = await prisma.transaction.findMany({
+        where: { userId }
+    });
+    return res.status(200).json(new ApiResponse(records, "Records retrieved successfully."));
+});
+
+const viewRecordById = asyncHandler(async (req, res) => {
+    const { id } = req.params;
+    const userId = req.user.id; 
+    const record = await prisma.transaction.findFirst({
+        where: { id, userId }
+    }); 
+    if (!record) {
+        return res.status(404).json(new ApiResponse(null, "Record not found."));
+    }   
+    return res.status(200).json(new ApiResponse(record, "Record retrieved successfully."));
+});
+
+const updateRecords = asyncHandler(async (req, res) => {
+    const { id } = req.params;
+    const { amount, type, category, date, description } = transactionSchema.parse(req.body);
+    const userId = req.user.id; 
+    const updatedRecord = await prisma.transaction.updateMany({
+        where: { id, userId },
+        data: {
+            amount,
+            type,
+            category,   
+            date: new Date(date),
+            description
+        }
+    });
+    return res.status(200).json(new ApiResponse(updatedRecord, "Record updated successfully."));
+}); 
+
+const deleteRecords = asyncHandler(async (req, res) => {
+    const { id } = req.params;
+    const userId = req.user.id; 
+    await prisma.transaction.delete({
+        where: { id, userId }
+    });
+    return res.status(200).json(new ApiResponse(null, "Record deleted successfully."));
+});
+
+export {createRecords, viewRecords, updateRecords, deleteRecords};
