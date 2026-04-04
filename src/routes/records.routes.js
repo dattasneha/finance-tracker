d79e3c82e9bd2b7d8ffefe7d63e9449882fe0062
Sneha Datta <snehadatta431@gmail.com>
2026-04-04 21:38:49 +0530

    Modify record router

--- src/routes/records.routes.js
+++ src/routes/records.routes.js
@@ -5,5 +5,8 @@ import { authorizeRoles } from "../middleware/role.middleware.js";
 const router = Router();
 
 router.route("/create").post(verifyUserJwt, authorizeRoles("ADMIN"), createRecords);
-
+router.route("/view").get(verifyUserJwt, authorizeRoles("ADMIN", "USER"), viewRecords);
+router.route("/view/:id").get(verifyUserJwt, authorizeRoles("ADMIN", "USER"), viewRecordById);
+router.route("/update/:id").put(verifyUserJwt, authorizeRoles("ADMIN"), updateRecords);
+router.route("/delete/:id").delete(verifyUserJwt, authorizeRoles("ADMIN"), deleteRecords);
 export default router;
