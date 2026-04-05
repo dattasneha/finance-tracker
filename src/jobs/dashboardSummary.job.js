b122ded05310f43a382528d0b8e807672fa93699
Sneha Datta <snehadatta431@gmail.com>
2026-04-05 22:48:35 +0530

    fix issues

--- src/jobs/dashboardSummary.job.js
+++ src/jobs/dashboardSummary.job.js
@@ -1,6 +1,13 @@
 import cron from "node-cron";
 import { prisma } from "../utils/prismaClient.js";
 
+function getWeekNumber(d) {
+  const date = new Date(d.getTime());
+  date.setHours(0, 0, 0, 0);
+  date.setDate(date.getDate() + 3 - ((date.getDay() + 6) % 7));
+  const week1 = new Date(date.getFullYear(), 0, 4);
+  return 1 + Math.round(((date - week1) / 86400000 - 3 + ((week1.getDay() + 6) % 7)) / 7);
+}
 export const dashBoardSummaryJob = () => {
   cron.schedule(process.env.JOB_SCHEDULE || "*/5 * * * *", async () => {
     try {
