61ff307cd39ca144c465b84e12dd70e73dffd6b0
Sneha Datta <snehadatta431@gmail.com>
2026-04-05 21:03:14 +0530

    Fix issues

--- src/routes/dashboard.routes.js
+++ src/routes/dashboard.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { getTotalIncome, getTotalExpenses, getNetWorth, getRecentActivities, getMonthlyTrends, getWeeklyTrends } from "../controllers/dashboard.controller.js";
+import { getDashBoardSummary,getdashboardSummaryByCategory, getRecentActivities, getMonthlyTrends, getWeeklyTrends } from "../controllers/dashboard.controller.js";
 import { verifyUserJwt } from "../middleware/auth.middleware.js";
 import { authorizeRoles } from "../middleware/role.middleware.js";
 const router = Router();
